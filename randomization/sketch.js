1a1cd6fab338b1c2ff631015a09190e9020fc565
Seungin Lyu <seungin.lyu@students.olin.edu>
2017-06-04 16:42:55 -0400

    Done

--- randomization/sketch.js
+++ randomization/sketch.js
@@ -13,47 +13,18 @@ function setup() {
 
   createCanvas(600, 600);
   background(255);
-  strokeWeight(3)
-  frameRate(2)
-  // //  drawCircle(width/2, height/2, 400)
-  // max_level = 10
-  // drawRect(0, 0, width, height, max_level)
+  noStroke();
+  frameRate(2);
+
 }
 
-
-function draw(){
-  strokeWeight(3)
-  //  drawCircle(width/2, height/2, 400)
-  max_level = 10
-  drawRect(0, 0, width, height, max_level)
-}
-
-
-function drawCircle(x, y, s){
-  fill(random(0, 200))
-  ellipse(x, y, s, s)
-  if(s > 0) {
-    drawCircle(x, y, s-10)
+function draw() {
+  var showRect = random(100) > 50;
+  if(showRect) {
+    fill(0);
   }
-}
-
-
-function drawRect(x, y, w, h, level){
-  hue = 10
-  fillHsluv(hue, 70, 20+level*10)
-  rect(x, y, w, h);
-
-  var splitWidth = random(1) > 0.5;
-  var split = random(0.3, 0.7);
-  if(level > 0){
-    if(splitWidth && w > 10){
-      drawRect(x, y, w * split, h, --level)
-      drawRect(w * split, y, (1 - split) * w, h, --level)
-    }
-    else if(h > 10){
-      level--;
-      drawRect(x, y, w, h * split, --level);
-      drawRect(x, y + (h * split), w, h * (1 - split), --level);
-    }
+  else{
+    fill(255, 0, 0)
   }
+  rect(0, 100, 100, 100)
 }
